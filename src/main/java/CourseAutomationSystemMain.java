b24c63b1fddac6c8dc2f9308ea48095610312c82
Ömer Faruk Yücetürk <oyuceturk@gtu.edu.tr>
2016-02-22 21:18:06 +0200

    Desing process...(Some methods in interfaces may be moved to the abstract classes.)

---
+++ src/main/java/CourseAutomationSystemMain.java
@@ -0,0 +1,5 @@
+/**
+ * Created by ömer on 22.2.2016.
+ */
+public class CourseAutomationSystemMain {
+}
--- src/main/java/UserInterface.java
+++ src/main/java/UserInterface.java
@@ -1,5 +1,27 @@
 /**
  * Created by ömer on 21.2.2016.
  */
+
 public interface UserInterface {
+    String getNameSurname();
+
+    void setNameSurname(String nameSurname);
+
+    int getNumber();
+
+    void setNumber(int number);
+
+    void listUserCourses();
+
+    boolean addUser(User theUser);
+
+    boolean removeUserByNameSurname(String userNameSurname);
+
+    boolean addCourse(Course theCourse);
+
+    boolean removeCourseByName(String courseName);
+
+    boolean addTutor(User theTutor);
+
+
 }
