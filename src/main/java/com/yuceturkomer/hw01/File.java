c220bf0e15a7b1c7a66e0ad3e5a51757ced210d5
yuceturkomer <oyuceturk@gtu.edu.tr>
2016-02-23 19:44:02 +0200

    Classes moved to the package

--- src/main/java/com/yuceturkomer/hw01/File.java
+++ src/main/java/com/yuceturkomer/hw01/File.java
@@ -1,3 +1,5 @@
+package com.yuceturkomer.hw01;
+
 /**
  * Created by ömer on 21.2.2016.
  */
---
+++ src/main/java/com/yuceturkomer/hw01/UserInterface.java
@@ -0,0 +1,23 @@
+package com.yuceturkomer.hw01;
+
+/**
+ * Created by ömer on 21.2.2016.
+ */
+
+public interface UserInterface {
+    String getNameSurname();
+
+    void setNameSurname(String nameSurname);
+
+    int getNumber();
+
+    String getMail();
+
+    void setMail(String mail);
+
+    void setNumber(int number);
+
+    void listUserCourses();
+
+}
+
