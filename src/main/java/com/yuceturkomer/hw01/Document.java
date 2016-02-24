0698a4e224757155db5aba7e5ec286bba4dd8cff
Ömer Faruk Yücetürk <oyuceturk@gtu.edu.tr>
2016-02-24 13:47:28 +0200

    Changes and stuff

--- src/main/java/com/yuceturkomer/hw01/Document.java
+++ src/main/java/com/yuceturkomer/hw01/Document.java
@@ -3,5 +3,28 @@ package com.yuceturkomer.hw01;
 /**
  * Created by ömer on 21.2.2016.
  */
-public class Document implements DocumentInterface{
+public abstract class Document implements DocumentInterface {
+    private String docType;
+    private String docName;
+
+    public Document(String docName, String docType){
+        setDocName(docName);
+        setDocType(docType);
+    }
+
+    public String getDocName() {
+        return docName;
+    }
+
+    public void setDocName(String docName) {
+        this.docName = docName;
+    }
+
+    public String getDocType() {
+        return docType;
+    }
+
+    public void setDocType(String docType) {
+        this.docType = docType;
+    }
 }
--- src/main/java/com/yuceturkomer/hw01/DocumentInterface.java
+++ src/main/java/com/yuceturkomer/hw01/DocumentInterface.java
@@ -4,4 +4,8 @@ package com.yuceturkomer.hw01;
  * Created by ömer on 21.2.2016.
  */
 public interface DocumentInterface {
+    String getDocName();
+    void setDocName(String docName);
+    String getDocType();
+    void setDocType(String docType);
 }
